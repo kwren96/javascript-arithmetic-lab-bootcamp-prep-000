0564bf7068e23ffe0a1605e6c2747de9f03a0b7f
Katherine Wren <kwren@outlook.com>
2019-03-23 13:52:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function subtract(a,b) {return a - b}
 function multiply(a,b) {return a*b}
 function divide(a,b) {return a/b}
 var n = 5
-function inc(n) {return n++}
-function dec(n) {return n--}
+function inc(n) {return ++n}
+function dec(n) {return --n}
 function makeInt(n) {return parseInt(n)}
