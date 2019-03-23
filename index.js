1050a8897c95418fa6d56459375a36fcdc5aa144
Katherine Wren <kwren@outlook.com>
2019-03-23 13:45:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function add(a,b) {return a + b}
 function subtract(a,b) {return a - b}
 function multiply(a,b) {return a*b}
 function divide(a,b) {return a/b}
-var n = 5
+n = 5
 function inc(n) {return n++}
 function dec(n) {return n--}
